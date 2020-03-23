19d18cd3ae58c3bddd4b8de84ee2eec401d8366d
Candra Brata <candrabrata8@gmail.com>
2020-03-23 15:46:26 +0800

    web desain (SBadmin bootstrap login, register,dashboard +Homepage MHS) + coonnect to framework CI

---
+++ application/views/auth/index.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Home</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="<?= base_url('assets/'); ?>css/bootstrap.min.css">
+  <link rel="stylesheet" href="<?= base_url('assets/'); ?>js/bootstrap.min.js">
+  <link href="<?= base_url('assets/'); ?>css\fontawesome-free\css\all.css" rel="stylesheet" type="text/css">
+  <link rel="stylesheet" href="<?= base_url('assets/'); ?>css/style.css">
+</head>
+<body>
+  <nav class="navbar sticky-top navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#"><h1><span>MHS</span>Center</h1></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+  
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+      </ul>
+      <ul class="navbar-nav navbar-right">
+        <li><a href="<?=base_url('auth/registration'); ?>" class="px-2"><button type="button" class="btn btn-primary"><i class="fas fa-door-open"></i> Register</button></a></li>
+        <li><a href="<?=base_url('auth/login'); ?>" class="px-2"><button type="button" class="btn btn-primary"><i class="fas fa-sign-in-alt"></i> Login</button></a></li>
+        
+      </ul>
+    </div>
+  </nav>
+  <!-- Header -->
+  <div class="container mt-3">
+    <div class="welcome">
+      <h1>WELCOME TO <span>MHS</span> INFORMATION CENTER</h1>
+    </div>
+      <img src="<?= base_url('assets/'); ?>images/logo/logoMHS.png" >
+  </div>
+  <!-- End of Header -->
+  <!-- Feature -->
+  <div class="container">
+    <h1 class="mb-4">Services</h1>
+    <div class="responsive">
+      <div class="gallery">
+        <a>
+            <div class="circle fadein">
+              <i class="fa fa-home"></i>
+            </div>
+        </a>
+          <h2>Setup residences</h2>
+          <div class="desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</div>
+      </div>
+    </div>
+    <div class="responsive">
+      <div class="gallery ">
+        <a>
+            <div class="circle fadein">
+              <i class="fa fa-folder"></i>
+            </div>
+        </a>
+          <h2>Apply Residence</h2>
+          <div class="desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
+      </div>
+    </div>
+    <div class="responsive">
+      <div class="gallery ">
+        <a>
+            <div class="circle fadein">
+              <i class="fa fa-location-arrow"></i>
+            </div>
+        </a>
+          <h2>Power Ranger</h2>
+          <div class="desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</div>
+      </div>
+    </div>
+    <div class="responsive">
+      <div class="gallery ">
+        <a>
+          <div class="circle fadein">
+            <i class="fa fa-comments"></i>
+          </div>
+        </a>
+          <h2>Ultramen Realgood</h2>
+          <div class="desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</div>
+      </div>
+    </div>
+    <div class="clearfix"></div>
+  </div>
+  <!-- End of Features -->
+  <div class="interest">
+    <h2>Are you interested?</h2>
+    <h3>Create your account now here !</h3>
+    <button class="btn- btn-primary"><a href="#">Click Here!</a></button>
+  </div>
+  <!-- Footer -->
+  <footer class="sticky-footer bg-white">
+    <div class="container my-auto">
+      <div class="copyright text-center my-auto">
+        <span>Copyright &copy; MHSCenter 2020</span>
+      </div>
+    </div>
+  </footer>
+  <!-- End of Footer -->
+  </footer>
+  <script src="<?= base_url('assets/'); ?>js/jquery-3.2.1.js"></script>  
+  <script src="<?= base_url('assets/'); ?>js/javascript.js"></script>
+</body>
+</html>

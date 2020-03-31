59e70af74e098e9ff055d49991469ca139e40d9c
Candra Brata <candrabrata8@gmail.com>
2020-03-31 17:04:16 +0800

    fix logout

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -169,7 +169,7 @@ class Auth extends CI_Controller {
     }
     public function logout()
     {
-        $this->session->unset_userdata('email');
+        $this->session->unset_userdata('username');
         $this->session->unset_userdata('role_id');
         $this->session->set_flashdata('message', '<div class="alert alert-success text-center alert-dismissible fade show" role="alert">You have been logged out! <button type="button" class="close" data-dismiss="alert" aria-label="Close">
         <span aria-hidden="true">&times;</span>
